20134f54be1687dd3c9fcfb3c127fc2bd85c6ae9
Nont Nonnipat <pinozen.thailand@gmail.com>
2022-07-26 13:12:39 +0700

    B-Hop system (didn't patch and setup for specific world

--- dev/src/main/java/pinont/server/minigame/events/Bhopping.java
+++ dev/src/main/java/pinont/server/minigame/events/Bhopping.java
@@ -1,28 +1,19 @@
 package pinont.server.minigame.events;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
-import org.bukkit.Location;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Bhopping implements Listener {
 
     @EventHandler
-    public void onPlayerJump(PlayerMoveEvent e) {
-        Location from = e.getFrom();
-        Location to = e.getTo();
+    public void onPlayerJump(PlayerJumpEvent e) {
         Player player = e.getPlayer();
-        player.setWalkSpeed(0.2F);
-        if (!(player.isJumping()) && !(player.isSprinting())) {
-            player.setWalkSpeed(0.2F);
-            player.sendMessage("not jump and not sprint");
-        } else if (from.getBlockY() == to.getBlockY() && player.isSprinting() && player.isJumping()){
-            player.setWalkSpeed(0.3F);
+        if (player.isSneaking() && player.isSprinting() && !(player.isFlying()) && !(player.isInLava()) ){
+            player.setVelocity(player.getLocation().getDirection());
             player.sendMessage("jump and sprint");
+
         }
     }
 }
