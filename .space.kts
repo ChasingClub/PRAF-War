507bca30b50e8178803f9ff0daae574bcce24c2f
Pinont PinozenTH <pinozen.thailand@gmail.com>
2022-08-04 16:37:10 +0000

    testMavenBuild

--- .space.kts
+++ .space.kts
@@ -4,15 +4,17 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Test-CommandExcuting") {
-    container(image = "openjdk:17") {
-        kotlinScript { api ->
-            if (api.gitBranch() == "refs/heads/master"){
-                println("Running in master branch")
-            }
-            else {
-                println("Running in custom branch")
-            }
+@file:DependsOn("pinont.server.minigame")
+
+import pinont.server.minigame.*
+
+job("Get example.com") {
+    container(image = "openjdk:11") {
+        kotlinScript {
+            val client = OkHttpClient()
+            val request = Request.Builder().url("http://example.com").build()
+            val response = client.newCall(request).execute()
+            println(response)
         }
     }
 }
